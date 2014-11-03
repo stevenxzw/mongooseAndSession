5b8de000aba444214a45b3717fb88b4dcf9a4856
xzw <stevenxzw@sina.com.cn>
2014-11-03 23:49:04 +0800

    impl

--- common/impl.js
+++ common/impl.js
@@ -4,11 +4,60 @@
 
 (function(){
 
+    var cutil = require('./../common/cutil').util;
+
     exports.Impl = {
 
-        get : function(param, res){
+        login : function(req, res){
+            var user = cutil.getHttpRequestParams(req);
+            if(user.username){
+                impl.getUserPwd(user.username, function(err, result, row){
+                    if(err){
+                        res.json(cutil.resultCollection(err,'', result));
+                    }else{
+                        if(impl.comparePwdMD5(result, user.pwd)){
+                            impl.setSessin(req, user);
+                            impl.uploadTable('users',{uid:user.username}, {$set:{'lastTime':+new Date,'logTimes':row['logTimes']+1}}, function(err){
+                                _debug && console.log('change user login:'+err);
+                            });
+                            res.json(cutil.resultCollection(err, '', result));
+                        }else{
+                            res.json(cutil.resultCollection("密码错误",'10001', result));
+                        }
+                    }
+                });
+            }
+        },
 
-        }
+        /*-----------------帐号功能------------------*/
+
+        //密码比较
+        comparePwdMD5 : function(dbpwd, pwd){
+            var crypto = require('crypto');
+            var md5 = crypto.createHash('md5');
+            return md5.update(pwd).digest('base64') === dbpwd;
+        },
+
+        //取出帐号密码
+        getUserPwd : function(uid, fn){
+            mongo.read('users', {"uid":uid}, function(err, result){
+                if(result.length){
+                    fn && fn(err, result[0].pwd, result[0]);
+                }else
+                    fn && fn("帐号不存在", result);
+            })
+        },
+
+
+        //获取帐号权限
+        getRole : function(uid, fn){
+            mongo.read('users', {"uid":uid}, function(err, result){
+                if(result.length){
+                    fn && fn(err, result[0].role);
+                }else
+                    fn && fn("帐号不存在", result);
+            })
+        },
     }
 
 });
